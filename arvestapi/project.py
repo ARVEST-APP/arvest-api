e784b3d5c165365abee0d639343ff69722966a01
Jacob <jacob.dchart@gmail.com>
2026-07-02 16:18:24 +0200

    Add arvest_version and update user_workspace handling

--- arvestapi/project.py
+++ arvestapi/project.py
@@ -10,12 +10,14 @@ class Project:
         """Represents an Arvest project."""
         
         self.debug = kwargs.get("debug", False)
+        
+        self.arvest_version = kwargs.get("arvest_version", None)
         self.id = kwargs.get("id", None)
         self.title = kwargs.get("title", None)
         self.owner_id = kwargs.get("owner_id", None)
         self.thumbnail_url = kwargs.get("thumbnail_url", None)
         self.description = kwargs.get("description", None)
-        self.user_workspace = kwargs.get("user_workspace", UserWorkspace())
+        self.user_workspace = kwargs.get("user_workspace", None)
         self.metadata = kwargs.get("metadata", None)
         self.created_at = kwargs.get("created_at", None)
         self.snapshot_hash = kwargs.get("snapshot_hash", None)
@@ -32,16 +34,79 @@ class Project:
         debug_print_response_body(response_body, self)
 
         self.id = response_body["project"]["id"]
+        self.arvest_version = response_body["project"]["arvestVersion"]
         self.title = response_body["project"]["title"]
         self.description = response_body["project"]["description"]
         self.thumbnail_url = response_body["project"]["thumbnailUrl"]
         self.owner_id = response_body["project"]["ownerId"]
-        self.user_workspace = UserWorkspace(response_body = response_body["project"]["userWorkspace"])
+        # self.user_workspace = UserWorkspace(response_body = response_body["project"]["userWorkspace"])
+        self.user_workspace = response_body["project"]["userWorkspace"]
         self.metadata = response_body["project"]["metadata"]
         self.created_at = response_body["project"]["created_at"]
         self.snapshot_hash = response_body["project"]["snapShotHash"]
         self.locked_by_user_id = response_body["project"]["lockedByUserId"]
         self.locked_at = response_body["project"]["lockedAt"]
+
+    def to_dict(self) -> dict:
+        return {
+            "project" : {
+                "arvestVersion" : self.arvest_version,
+                "id" : self.id,
+                "title" : self.title,
+                "description" : self.description,
+                "thumbnailUrl" : self.thumbnail_url,
+                "ownerId" : self.owner_id,
+                "userWorkspace" : self.user_workspace,
+                "metadata" : self.metadata,
+                "created_at" : self.created_at,
+                "snapShotHash" : self.snapshot_hash,
+                "lockedByUserId" : self.locked_by_user_id,
+                "lockedAt" : self.locked_at
+            },
+            "group" : self._arvest_instance._personal_group.to_dict()
+        }
+
+    def _update_distant_from_self(self) -> None:
+        """Update the object on the server from current python object."""
+
+        # TODO Add updated at parsing to this!
+
+        if self._arvest_instance != None:
+                
+            url = f"{self._arvest_instance._arvest_prefix}/link-group-project/updateProject"
+            payload = self.to_dict()
+
+            response = requests.patch(url, headers = self._arvest_instance._auth_header, json = payload)
+
+            if response.status_code == 200:
+                pass
+            else:
+                print("Unable to update manifest.")
+                print(f"Status code: {response.status_code}. Response: {response.json()}")
+                return None
+        else:
+            print("Unable to update Manifest as there is no known Arvest instance context.")
+
+    def update_title(self, new_title : str) -> None:
+        """Update the title of the Manifest object."""
+        self.title = new_title
+        self._update_distant_from_self()
+
+    def update_description(self, new_description : str) -> None:
+        """Update the desciption of the Media object."""
+        self.description = new_description
+        self._update_distant_from_self()
+
+    def update_thumbnail_url(self, new_thumbnail : str) -> None:
+        """Update the thumbnail url of the Manifest object."""
+        self.thumbnail_url = new_thumbnail
+        self._update_distant_from_self()
+
+    def update_workspace(self, new_workspace : str) -> None:
+        """Update the workspace of the Manifest object."""
+        self.arvest_version = 2
+        self.user_workspace = new_workspace
+        self._update_distant_from_self()
 
     def remove(self):
         url = f"{self._arvest_instance._arvest_prefix}/link-group-project/delete/project/{self.id}"  
